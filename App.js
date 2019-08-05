26bdff06195396b7558dc85ecc44148890438bc0
yuki nara <yukinara@NYnoMacBook-Air.local>
2019-08-05 14:57:16 +0900

    更新

--- App.js
+++ App.js
@@ -16,30 +16,4 @@ export default class reduxApp extends Component {
       </Provider>
     )
   }
-}
-
-
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>HEllO!I'll never forget the excitement i had at that time.</Text>
-//         <Text>HEllO!I'll never forget the excitement i had at that time.</Text>
-//         <Text>HEllO!I'll never forget the excitement i had at that time.</Text>
-//         <Text>HEllO!I'll never forget the excitement i had at that time.</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+};
